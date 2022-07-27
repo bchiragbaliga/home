a276391417dd8b8e11cad9985654f36809cb2bb3
bchiragbaliga <bchiragbaliga.is19@rvce.edu.in>
2022-07-27 22:59:19 +0530

    Updates

---
+++ precache-manifest.bb864b32d39675a42556bfc81bddc5e8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9447d8ba2b8812eb7b0e725279af762e",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "58b0117e3f33ab954de2",
+    "url": "/home/static/css/main.ab588325.chunk.css"
+  },
+  {
+    "revision": "94833f0d9aaa3be6210f",
+    "url": "/home/static/js/2.19e99e5d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/home/static/js/2.19e99e5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58b0117e3f33ab954de2",
+    "url": "/home/static/js/main.3ed5175d.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "f22fc652436b28f7ef3c046230ff0bf2",
+    "url": "/home/static/media/chiragbaliga.f22fc652.jpg"
+  },
+  {
+    "revision": "5f83a157e4352e0784fe7afdecd939b6",
+    "url": "/home/static/media/resume.5f83a157.pdf"
+  }
+]);
